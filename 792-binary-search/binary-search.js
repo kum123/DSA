5a60c728072b832ead538dba5af8febf06127d14
kum123 <kumar.oneforce@gmail.com>
2023-09-08 12:10:36 +0530

    Time: 57 ms (59.56%) | Memory: 45.2 MB (35.68%) - LeetSync

---
+++ 792-binary-search/binary-search.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+    
+
+             let left = 0; let right = nums.length - 1;
+        
+        // Under this condition
+        while (left <= right) {
+            // Get the middle index and the middle value.
+            let mid = Math.floor((left +right)/2);
+            
+            // Case 1, return the middle index.
+            if (nums[mid] == target) {
+                return mid;
+            } 
+            // Case 2, discard the smaller half.
+            else if (nums[mid] < target) {
+                left = mid + 1;   
+            } 
+            // Case 3, discard the larger half.
+            else {
+                right = mid - 1;
+            }
+        }
+        return -1;
+
+ 
+
+};
