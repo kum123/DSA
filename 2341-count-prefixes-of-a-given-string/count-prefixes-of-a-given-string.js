ce3a314eb0e7d7cd1d7cf7cd0268a1cad142676a
kum123 <kumar.oneforce@gmail.com>
2023-10-09 16:04:19 +0530

    Time: 47 ms (93.66%) | Memory: 42.9 MB (43.44%) - LeetSync

--- 2341-count-prefixes-of-a-given-string/count-prefixes-of-a-given-string.js
+++ 2341-count-prefixes-of-a-given-string/count-prefixes-of-a-given-string.js
@@ -5,11 +5,12 @@
  */
 var countPrefixes = function(words, s) {
     
-    let sum=0;
-for( let i=0; i<words.length ;i++){
-if(s.startsWith(words[i])){
-sum++
-}
-}
-return sum
+//     let sum=0;
+// for( let i=0; i<words.length ;i++){
+// if(s.startsWith(words[i])){
+// sum++
+// }
+// }
+// return sum
+return words.filter((x) => s.startsWith(x)).length;
 };
