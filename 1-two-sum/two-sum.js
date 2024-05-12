3a2017adf9954ce8307670a4364048342887f70c
kum123 <kumar.oneforce@gmail.com>
2024-05-12 12:57:56 +0530

    Time: 59 ms (70.73%) | Memory: 51 MB (36.57%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -4,57 +4,39 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
- const map = {};
+//  const map = {};
 
-  for (let i = 0; i < nums.length; i++) {
-    const another = target - nums[i];
+//   for (let i = 0; i < nums.length; i++) {
+//     const another = target - nums[i];
 
-    if (another in map) {
-      return [map[another], i];
+//     if (another in map) {
+//       return [map[another], i];
+//     }
+
+//     map[nums[i]] = i;
+//   }
+
+//   return null;
+
+
+
+
+    let hashMap=new Map();
+    for(let i=0; i < nums.length;i++){
+        if(hashMap[nums[i]] >= 0){
+        return [hashMap[nums[i]],i];
+        }else{
+        hashMap[target - nums[i]] = i;
     }
+     }
 
-    map[nums[i]] = i;
-  }
-
-  return null;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    // let hashMap={};
-    // for(let i=0; i < nums.length;i++){
-    //     if(hashMap[nums[i]] >= 0){
-    //     return [hashMap[nums[i]],i];
-    //     }else{
-    //     hashMap[target - nums[i]] = i;
-    // }
-    //  }
-   
+  // for(let i=0; i < nums.length;i++){
+  //       if(hashMap[nums[i]] >= 0){
+  //       return [hashMap[nums[i]],i];
+  //       }else{
+  //       hashMap[target - nums[i]] = i;
+  //   }
+  //    }
+   return null;
  };
 
