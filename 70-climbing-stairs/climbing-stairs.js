ae4423a89a9bfae24a7763523f769c997fc12811
kum123 <kumar.oneforce@gmail.com>
2024-05-22 11:46:17 +0530

    Time: 43 ms (90.79%) | Memory: 48.8 MB (43.10%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.js
@@ -0,0 +1,35 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function(N) {
+    
+    // to reach till stairs upto maximum two ways ways (0->1-2)/(0->2 direct jump) 2 
+   
+    // DP approach 
+    let memorization = {};
+    function ways(N){
+         if( N == 1 || N == 2 ){
+        return N;
+    }
+    if(memorization[N]) return memorization[N];
+    let ans = ways(N-1) + ways(N-2);
+    memorization[N] = ans;
+    return memorization[N];
+    }
+    return ways(N);
+
+    //      if( N == 1 || N == 2 ){
+    //     return N;
+    // }
+    // let stairs = Array(N+1).fill();
+    // stairs[0] = 1;
+    // stairs[1] = 1;
+    // for(let i=2; i <=N ;i++){
+    //     stairs[i] = stairs[i-1] + stairs[i-2];
+    // }
+    // return stairs[N];
+    
+    
+
+};
