11a58f66564b3b5829e8df3a2514bb29c6a2f637
kum123 <kumar.oneforce@gmail.com>
2024-06-28 19:02:28 +0530

    Time: 107 ms (39.56%) | Memory: 50.5 MB (85.20%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.js
+++ 5-longest-palindromic-substring/longest-palindromic-substring.js
@@ -10,23 +10,65 @@
 
 var longestPalindrome = function(str) {
     
-    if(str.length < 1 || str === null) return '';
-    let longest = '';
 
-    for(let i = 0; i < str.length; i++) {
+ var max = '';
+  for (var i = 0; i < str.length; i++) {
+    // this loop is to take into account 
+    // different palindromes like 'aba' and 'abba'
+    for (var j = 0; j < 2; j++) {
+      var left = i;
+      var right = i + j;
+      while (str[left] && str[left] === str[right]) {
+        left--;
+        right++;
+      }
+      // here imagine we get into string like
+      // "sabbad", then
+      // right = 5
+      // left = 0
+      // and actual length of "abba" should be "4"
+      // 5 - 0 - 1 === 4
+      if ((right - left - 1) > max.length) {
+        max = str.substring(left + 1, right);
+      }
+        
+    }
+  }
+  return max;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//     if(str.length < 1 || str === null) return '';
+//     let longest = '';
+
+//     for(let i = 0; i < str.length; i++) {
                     
                
-                let oddPolindrom = expandFromCenter(str,i-1,i+1);
-                let evenPolindrom = expandFromCenter(str,i-1,i);
-                if(oddPolindrom.length > longest.length){
-                    longest = oddPolindrom;
-                }
-                if(evenPolindrom.length > longest.length){
-                    longest = evenPolindrom;
-                }
+//                 let oddPolindrom = expandFromCenter(str,i-1,i+1);
+//                 let evenPolindrom = expandFromCenter(str,i-1,i);
+//                 if(oddPolindrom.length > longest.length){
+//                     longest = oddPolindrom;
+//                 }
+//                 if(evenPolindrom.length > longest.length){
+//                     longest = evenPolindrom;
+//                 }
                 
-        }
-return longest;
+//         }
+// return longest;
    
 };
 
