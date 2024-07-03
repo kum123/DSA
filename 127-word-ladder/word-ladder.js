d85a31214b1c503947ab9eb8325d425c51f87e64
kum123 <kumar.oneforce@gmail.com>
2024-07-03 11:40:47 +0530

    Time: 215 ms (49.39%) | Memory: 59.1 MB (43.66%) - LeetSync

--- 127-word-ladder/word-ladder.js
+++ 127-word-ladder/word-ladder.js
@@ -8,36 +8,67 @@ var ladderLength = function(beginWord, endWord, wordList) {
     
 
            
-   if (!wordList.includes(endWord)) {
-        return 0;
-    }
+//    if (!wordList.includes(endWord)) {
+//         return 0;
+//     }
     
-    const wordSet = new Set(wordList);
-    const dp = {};
-    dp[beginWord] = 1;
+//     const wordSet = new Set(wordList);
+//     const dp = {};
+//     dp[beginWord] = 1;
 
-    const queue = [beginWord];
+//     const queue = [beginWord];
 
-    while (queue.length > 0) {
-        const currentWord = queue.shift();
+//     while (queue.length > 0) {
+//         const currentWord = queue.shift();
 
-        for (let i = 0; i < currentWord.length; i++) {
-            const wordArray = currentWord.split('');
-            for (let j = 0; j < 26; j++) {
-                wordArray[i] = String.fromCharCode(97 + j); // Generating new words by changing one character
-                const newWord = wordArray.join('');
+//         for (let i = 0; i < currentWord.length; i++) {
+//             const wordArray = currentWord.split('');
+//             for (let j = 0; j < 26; j++) {
+//                 wordArray[i] = String.fromCharCode(97 + j); // Generating new words by changing one character
+//                 const newWord = wordArray.join('');
 
-                if (wordSet.has(newWord) && !dp.hasOwnProperty(newWord)) {
-                    dp[newWord] = dp[currentWord] + 1;
-                    if (newWord === endWord) {
-                        return dp[newWord];
+//                 if (wordSet.has(newWord) && !dp.hasOwnProperty(newWord)) {
+//                     dp[newWord] = dp[currentWord] + 1;
+//                     if (newWord === endWord) {
+//                         return dp[newWord];
+//                     }
+//                     queue.push(newWord);
+//                 }
+//             }
+//         }
+//     }
+
+//     return 0;
+
+    const wordSet = new Set(wordList)
+    let queue = [beginWord];
+    let steps = 1;
+    
+    while(queue.length) {
+        const next = [];
+        
+        // loop over each word in the queue
+        for(let word of queue) {
+            if(word === endWord) return steps;
+            
+            // loop over each char of the word 
+            for(let i = 0; i < word.length; i++) {
+                
+                // and replace the char with letters from [a - z]
+                for(let j = 0; j < 26; j++) {
+                    const newWord = word.slice(0, i) + String.fromCharCode(j + 97) + word.slice(i+1);
+                    
+                    // if the new word exist in the word list add it to the queue
+                    if(wordSet.has(newWord)) {
+                        next.push(newWord);
+                        wordSet.delete(newWord);
                     }
-                    queue.push(newWord);
                 }
             }
         }
+        queue = next
+        steps++;
     }
-
-    return 0;
+    return 0;    
 
 };
