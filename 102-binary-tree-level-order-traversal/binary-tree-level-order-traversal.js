5e8b80f1952ad8b91c0aa0bad251930b520ccc54
kum123 <kumar.oneforce@gmail.com>
2024-07-09 22:13:25 +0530

    Time: 78 ms (54.77%) | Memory: 55.9 MB (11.37%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.js
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.js
@@ -11,24 +11,61 @@
  * @return {number[][]}
  */
 var levelOrder = function(root) {
-    
+
                 if(!root) return [];
-                let queue = [root,null], ans = [[]], row =0;
+
+                let queue = [root,null]; let ans = [[]]; let row = 0;
 
                 while(queue.length > 1){
-                        
-                        let cur = queue.shift();
-                        if(cur == null){
-                         row++;
+
+                    let cur = queue.shift();
+                    if(cur == null){
+                        row++;
                         ans[row] = [];
                         queue.push(null);
-                        } else {
-                            ans[row].push(cur.val);
-                       if(cur.left) queue.push(cur.left) ;
-                       if(cur.right) queue.push(cur.right)   
-                        }
-                    
-                       }
+                    } else {
+                        ans[row].push(cur.val);
+                        if(cur.left) queue.push(cur.left);
+                        if(cur.right) queue.push(cur.right);
+                    }
+                }
 
-            return ans;
+
+        return ans;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+            //     if(!root) return [];
+            //     let queue = [root,null], ans = [[]], row =0;
+            //     // 
+            //     while(queue.length > 1){
+                        
+            //             let cur = queue.shift();
+            //             if(cur == null){
+            //              row++;
+            //             ans[row] = [];
+            //             queue.push(null);
+            //             } else {
+            //                 ans[row].push(cur.val);
+            //            if(cur.left) queue.push(cur.left) ;
+            //            if(cur.right) queue.push(cur.right)   
+            //             }
+                    
+            //            }
+
+            // return ans;
 };
