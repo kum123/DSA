0d84e0670ffab4512546768e5a030472eb5d81a7
kum123 <kumar.oneforce@gmail.com>
2024-07-20 08:01:46 +0530

    Time: 83 ms (83.87%) | Memory: 63.1 MB (87.00%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.js
+++ 219-contains-duplicate-ii/contains-duplicate-ii.js
@@ -5,12 +5,20 @@
  */
 var containsNearbyDuplicate = function (nums, k) {
 
-     const map = new Map();
-  for (let i = 0; i < nums.length; i++) {
-    if (i - map.get(nums[i]) <= k) {
-      return true;
+//      const map = new Map();
+//   for (let i = 0; i < nums.length; i++) {
+//     if (i - map.get(nums[i]) <= k) {
+//       return true;
+//     }
+//     map.set(nums[i], i);
+//   }
+//   return false;
+  let set = new Set();
+    
+    for (let i = 0; i < nums.length; i++) {
+        if (set.has(nums[i])) return true;
+        set.add(nums[i]);
+        if (set.size > k) set.delete(nums[i - k]);
     }
-    map.set(nums[i], i);
-  }
-  return false;
+    return false;
 };
