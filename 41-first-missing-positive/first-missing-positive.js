17fd6292ab7eb71d969f8f95c39534e1c399701a
kum123 <kumar.oneforce@gmail.com>
2024-08-25 16:50:54 +0530

    Time: 99 ms (31.27%) | Memory: 76.8 MB (5.33%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.js
+++ 41-first-missing-positive/first-missing-positive.js
@@ -3,27 +3,35 @@
  * @return {number}
  */
 var firstMissingPositive = function(nums) {
-    const swap = (arr, i, j) => {
-        [arr[i], arr[j]] = [arr[j], arr[i]];
-    };
+    // const swap = (arr, i, j) => {
+    //     [arr[i], arr[j]] = [arr[j], arr[i]];
+    // };
     
-    const n = nums.length;
+    // const n = nums.length;
     
-    // Place each positive integer i at index i-1 if possible
-    for (let i = 0; i < n; i++) {
-        while (nums[i] > 0 && nums[i] <= n && nums[i] !== nums[nums[i] - 1]) {
-            console.log("nums[i]",nums[i]," nums[nums[i] - 1]", nums[nums[i] - 1])
-            swap(nums, i, nums[i] - 1);
-        }
+    // // Place each positive integer i at index i-1 if possible
+    // for (let i = 0; i < n; i++) {
+    //     while (nums[i] > 0 && nums[i] <= n && nums[i] !== nums[nums[i] - 1]) {
+    //         console.log("nums[i]",nums[i]," nums[nums[i] - 1]", nums[nums[i] - 1])
+    //         swap(nums, i, nums[i] - 1);
+    //     }
+    // }
+    
+    // // Find the first missing positive integer
+    // for (let i = 0; i < n; i++) {
+    //     if (nums[i] !== i + 1) {
+    //         return i + 1;
+    //     }
+    // }
+    
+    // // If all positive integers from 1 to n are present, return n + 1
+    // return n + 1;
+    let m = new Map();
+    for (let i = 0; i < nums.length; i++) {
+        m.set(nums[i], 1);
     }
-    
-    // Find the first missing positive integer
-    for (let i = 0; i < n; i++) {
-        if (nums[i] !== i + 1) {
-            return i + 1;
-        }
+    for (let i = 1; i <= nums.length; i++) {
+        if (!m.has(i)) return i;
     }
-    
-    // If all positive integers from 1 to n are present, return n + 1
-    return n + 1;
+    return nums.length + 1;
 };
