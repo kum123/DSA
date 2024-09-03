212040836eabc5b343f160410182ed8746ac3a04
kum123 <kumar.oneforce@gmail.com>
2024-09-03 18:16:28 +0530

    Time: 61 ms (71.31%) | Memory: 52.1 MB (70.20%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
@@ -4,19 +4,33 @@
  */
 var removeDuplicates = function(nums) {
     
-   let i=0;
+//    let i=0;
      
-    for (let j=1; j < nums.length;j++){
+//     for (let j=1; j < nums.length;j++){
     
-        if(nums[i] === nums[j]){
+//         if(nums[i] === nums[j]){
 
-                 nums.splice(j,1);
-                  j--;
-            }
-        else{
-                i++;
-            }
+//                  nums.splice(j,1);
+//                   j--;
+//             }
+//         else{
+//                 i++;
+//             }
+//     }
+
+     if (nums.length === 0) {
+        return 0;
     }
+
+    let k = 1; // Initialize the count of unique elements to 1
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] !== nums[k - 1]) {
+            nums[k] = nums[i]; // Overwrite the next unique element
+            k++;
+        }
+    }
+
+    return k;
    
  
 };
