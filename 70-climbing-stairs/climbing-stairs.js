f6f7375a2f2f03f1cc14ce9d9792f373394bfd36
kum123 <kumar.oneforce@gmail.com>
2024-09-04 20:59:42 +0530

    Time: 59 ms (13.21%) | Memory: 48.9 MB (24.94%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.js
+++ 70-climbing-stairs/climbing-stairs.js
@@ -7,17 +7,17 @@ var climbStairs = function(N) {
     // to reach till stairs upto maximum two ways ways (0->1-2)/(0->2 direct jump) 2 
    
     // DP approach 
-    let memorization = {};
-    function ways(N){
-         if( N == 1 || N == 2 ){
-        return N;
-    }
-    if(memorization[N]) return memorization[N];
-    let ans = ways(N-1) + ways(N-2);
-    memorization[N] = ans;
-    return memorization[N];
-    }
-    return ways(N);
+    //let memorization = {};
+    //function ways(N){
+    //      if( N == 1 || N == 2 ){
+    //     return N;
+    // }
+    // if(memorization[N]) return memorization[N];
+    // let ans = ways(N-1) + ways(N-2);
+    // memorization[N] = ans;
+    // return memorization[N];
+    // }
+    // return ways(N);
 
     //      if( N == 1 || N == 2 ){
     //     return N;
@@ -26,10 +26,18 @@ var climbStairs = function(N) {
     // stairs[0] = 1;
     // stairs[1] = 1;
     // for(let i=2; i <=N ;i++){
+         
+
     //     stairs[i] = stairs[i-1] + stairs[i-2];
     // }
     // return stairs[N];
-    
-    
+    if(N == 1 || N == 2) return N;
+    let dp = new Array(N+1).fill(0);
+    dp[0] = 1;
+    dp[1] = 1;
+    for(let i=2; i <= N;i++){
+        dp[i] = dp[i-1] + dp[i-2];
+    }
+    return dp[N];
 
 };
